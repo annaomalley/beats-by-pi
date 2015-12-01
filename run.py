2060057fbd3aee1e073376d06c6d3eebf2b97925
Brian Tong <brktong@gmail.com>
2015-12-01 14:08:57 -0500

    Update message options

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import random
 import os
 import subprocess
 import youtube_search
@@ -14,34 +15,43 @@ def get_action():
 @app.route('/', methods=['POST'])
 def post_action():
     body = request.form.get('Body')
-    if body == "#stop":
-        subprocess("echo 0 > autoplay.txt", shell=True)
+    if body =='#stop'
+        change_mode('0')
         os.system('pkill vlc')
-    elif body == "#play":
-        subprocess.Popen("python play.py", shell=True)
-    elif body[0:2] == "#q":
-        body = body[2:]
-        cmd = "echo %s >> playlist.txt" % body
-        subprocess.Popen(cmd, shell=True)
-    elif body[0] == "#":
-        subprocess.Popen("echo [Raspberry] Error: Invalid Command", shell=True)
-        raise ValueError("Invalid Command")
-    else:  
-        with open('current_song.txt', 'w') as songfile:
-            songfile.write(body)
-        url = youtube_search.search(body)
-        subprocess("echo 0 > autoplay.txt", shell=True)
+    elif body == '#queue'
+        change_mode('1')
         os.system('pkill vlc')
-        print(url)
-        cmd = "youtube-dl -f 140 -o - " + url + "| vlc-wrapper --play-and-exit --novideo --intf dummy -; python play.py"
-        subprocess.Popen(cmd, shell=True)
-    return "success"
+        subprocess.Popen('python play.py') 
+    else
+        if check_mode() == '0'
+            url = youtube_search.search(body)
+            os.system('pkill vlc')
+            cmd = "youtube-dl -f 140 -o - " + url + "| vlc-wrapper --play-and-exit --novideo --intf dummy -"
+            subprocess.Popen(cmd, shell=True)
+        else
+            fo = open('playlist.txt', 'a')
+            fo.write(body + '\n')
+            fo.close()
+    return 0
+
+def change_mode(mode):
+    fo = open('autoplay.txt', 'w')
+    fo.write(mode)
+    fo.close();
+
+def check_mode():
+    fo = open('autoplay.txt', 'r')
+    mode = fo.read(2)
+    fo.close()
+    return mode
 
 @app.route('/error', methods=['GET','POST'])
 def error_action():
     return render_template('reply_error.xml')
 
-
 if __name__ == '__main__':
-    subprocess.Popen("> playlist.txt; echo 1 > autoplay.txt", shell=True)
+    open('playlist.txt', 'w').close()
+    fo = open('autoplay.txt', 'w')
+    fo.write('0')
+    fo.close()
     app.run(host='0.0.0.0', port=80)
